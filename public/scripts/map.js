13bc3aba4eed04054f5c4d7076f5247b994acb42
Carmen <sdluojm@gmail.com>
2019-09-20 22:17:13 +0000

    add map

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -3,11 +3,11 @@ let map;
 function initMap() {
   // The location of Uluru
   var toronto = {lat: 43.7, lng: -79.4};
-  let foodtruck1 = {lat: 43.8, lng: -79.4};
+  let foodtruck1 = {lat: 43.8, lng: -77.4};
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 12, center: toronto});
+      document.getElementById('map'), {zoom: 8, center: toronto});
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: toronto, map: map});
-  let foodtruck  = new google.maps.Marker({position: foodtruck1, map: map})
+  let foodtruck  = new google.maps.Marker({position: foodtruck1, map: map});
 }
