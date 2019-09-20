697702b1d9aa8404f06694019092e51e53f6ec10
allisonnnnnn <allisonchen1130@gmail.com>
2019-09-20 19:21:46 -0400

    changed app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -33,3 +33,38 @@
 //     }
 //   });
 // });
+function appendMap() {
+  return `<div>
+  <script type="text/javascript" src="/scripts/map.js"></script>
+  <script async defer
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZImsQ1Qw68YIf_tHVOoMhs5wz5-F4JHA&callback=initMap">
+  </script>
+  </div>`;
+}
+
+$(function() {
+  $.ajax({
+    method: "GET",
+    url: "/api/users"
+  }).done(users => {
+    for (user of users) {
+      $("<div>")
+        .text(user.name)
+        .appendTo($("body"));
+    }
+  });
+  // console.log('do i work??');
+  // // appendMap();
+  // let $map = appendMap();
+  // console.log($map);
+  // let $body = $("body");
+  // console.log($body);
+  // $body.append($map);
+
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/"
+  // }).done(() => {
+
+  // });
+});
