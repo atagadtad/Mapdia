6847c860bdb513950820cd06b743d1bfac9639e2
allisonnnnnn <allisonchen1130@gmail.com>
2019-09-20 18:22:13 -0400

    layout.css and index.ejs edited'

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,35 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+// const userItemView = user => {
+//   const $item = $(`<div class='user'>${user.email}</div>`);
+//   $item.on("click", () => alert("hello" + user.email));
+//   return $item;
+// };
+
+// const userPageView = users => {
+//   const $page = $(`<div class='page'></div>`);
+//   for (user of users) {
+//     $page.append(userItemView(user));
+//   }
+//   return $page;
+// };
+
+// const loadUserPage = () => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done(response => {
+//     $("main").html(userPageView(response.users));
+//   });
+// };
+
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done(users => {
+//     for (user of users) {
+//       $("<div>")
+//         .text(user.name)
+//         .appendTo($("body"));
+//     }
+//   });
+// });
