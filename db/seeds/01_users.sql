96cd3a009cb6cbb4df489f556d711920fcc526af
atagadtad <aarontagadtad@gmail.com>
2019-09-20 17:07:33 +0000

    changed 01_users.sql seeds to match the tables created the the 01_users.sql schema

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,12 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users
+  (email, password)
+VALUES
+  ('sebastianguerra@ymail.com', 'password');
+INSERT INTO users
+  (email, password)
+VALUES('jacksonrose@hotmail.com', 'password');
+INSERT INTO users
+  (email, password)
+VALUES('victoriablackwell@outlook.com', 'password');
+
