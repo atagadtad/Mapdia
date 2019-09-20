9ed5bbd33838008019acae5685c3a2aa7682472e
atagadtad <aarontagadtad@gmail.com>
2019-09-20 20:30:34 +0000

    added seeds to ./seeds for users, pins, maps tables

---
+++ db/seeds/02_maps.sql
@@ -0,0 +1,9 @@
+INSERT INTO maps
+  (owner_id, description)
+VALUES(1, 'Sight-seeing sites to see while camping');
+INSERT INTO maps
+  (owner_id, description)
+VALUES(2, 'Italian restaurants I''d like to eat at');
+INSERT INTO maps
+  (owner_id, description)
+VALUES(3, 'Wedding venues to scout out');
---
+++ db/seeds/03_pins.sql
@@ -0,0 +1,12 @@
+INSERT INTO pins
+  (longitude, latitude, map_id)
+VALUES
+  (43.644114, 79.402328, 3);
+INSERT INTO pins
+  (longitude, latitude, map_id)
+VALUES
+  (43.644999, 79.400558, 2);
+INSERT INTO pins
+  (longitude, latitude, map_id)
+VALUES
+  (43.643470, 79.399206, 1);
