5b1a7107dc279dbdcde4df7eff8c10be7487edbd
atagadtad <aarontagadtad@gmail.com>
2019-09-20 22:11:35 +0000

    added dragging feature to markers on google maps

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -2,12 +2,20 @@ let map;
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var toronto = {lat: 43.7, lng: -79.4};
-  let foodtruck1 = {lat: 43.8, lng: -77.4};
+  var toronto = { lat: 43.7, lng: -79.4 };
+  let foodtruck1 = { lat: 43.8, lng: -77.4 };
   // The map, centered at Uluru
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 8, center: toronto});
+    document.getElementById('map'), { zoom: 8, center: toronto });
   // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: toronto, map: map});
-  let foodtruck  = new google.maps.Marker({position: foodtruck1, map: map});
+  var marker = new google.maps.Marker({
+    position: toronto,
+    map: map,
+    draggable: true
+  });
+  let foodtruck = new google.maps.Marker({
+    position: foodtruck1,
+    map: map,
+    draggable: true
+  });
 }
