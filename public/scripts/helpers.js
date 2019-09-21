068bb975df07aa8ad34d4e9c02072c251f50e6d3
Carmen <sdluojm@gmail.com>
2019-09-21 18:09:43 +0000

    add search bar into map and seperate it into map.js

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -4,11 +4,65 @@ function appendMap() {
   <script async defer
   src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZImsQ1Qw68YIf_tHVOoMhs5wz5-F4JHA&libraries=places&callback=initMap">
   </script>
+  </div>
+  <div>
+  <button id = 'searchPlace'>Find place</button>
   </div>`;
   return $map;
 }
 
 function appendSearch() {
-  return `<input id="search-input" class="controls" type="text" placeholder="Search Box"></input>`;
+  console.log(' i am appending');
+  let map = document.getElementById('map').gMap;
+  console.log(map);
+ // return `<input id="search-input" class="controls" type="text" placeholder="Search Box"></input>`;
+ // let map = document.getElementById('map');
+
+  $("body").append(`<input id="search-input" class="controls" type="text" placeholder="Search Box" autocomplete='on'></input>`);
+  let searchInput = document.getElementById('search-input');
+  let searchBox = new google.maps.places.SearchBox(searchInput);
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(searchInput);
+  map.addListener('bounds_changed', function () {
+    searchBox.setBounds(map.getBounds());
+  });
+  //after put in search locations
+  let mapMarkers = [];
+  searchBox.addListener('places_changed', function () {
+    let places  = searchBox.getPlaces();
+    if (places.length === 0) {
+      return;
+    }
+
+    //Clear out the old markers
+    mapMarkers.forEach(function (marker) {
+      marker.setMap(null);
+    });
+    mapMarkers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function (place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      mapMarkers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+ //     mapMarkers.forEach(marker => createMarker(marker));
+    })
+  });
 }
 export { appendMap, appendSearch };
--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,19 +1,13 @@
-let map;
+let googleMap;
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
   var toronto = { lat: 43.7, lng: -79.4 };
-  let foodtruck1 = { lat: 43.8, lng: -77.4 };
   // The map, centered at Uluru
   var map = new google.maps.Map(
     document.getElementById('map'), { zoom: 8, center: toronto });
   var marker = new google.maps.Marker({
     position: toronto,
-    map: map,
-    draggable: true
-  });
-  let foodtruck = new google.maps.Marker({
-    position: foodtruck1,
     map: map,
     draggable: true
   });
@@ -24,7 +18,6 @@ function initMap() {
   }
   placeService.findPlaceFromQuery(request, (results, status) => {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
-
       results.forEach((item) => {
         // console.log(item)
         // place_id, name, formatted_address, geometry.location, icon
@@ -33,52 +26,9 @@ function initMap() {
 
     }
   });
-  //searcn input
-  let searchInput = document.getElementById('search-input');
-  let searchBox = new google.maps.places.SearchBox(searchInput);
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(searchInput);
-  map.addListener('bounds_changed', function () {
-    searchBox.setBounds(map.getBounds());
-  });
-  //after put in search locations
-  let mapMarkers = [];
-  searchBox.addListener('places_changed', function () {
-    let places  = searchBox.getPlaces();
-    if (places.length === 0) {
-      return;
-    }
+let divMap = document.getElementById('map');
+divMap.gMap = map;
 
-    //Clear out the old markers
-    mapMarkers.forEach(function (marker) {
-      marker.setMap(null);
-    });
-    mapMarkers = [];
-
-    // For each place, get the icon, name and location.
-    var bounds = new google.maps.LatLngBounds();
-    places.forEach(function (place) {
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-      var icon = {
-        url: place.icon,
-        size: new google.maps.Size(71, 71),
-        origin: new google.maps.Point(0, 0),
-        anchor: new google.maps.Point(17, 34),
-        scaledSize: new google.maps.Size(25, 25)
-      };
-
-      // Create a marker for each place.
-      mapMarkers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
- //     mapMarkers.forEach(marker => createMarker(marker));
-    })
-  });
 }
 
 function createMarker(place) {
