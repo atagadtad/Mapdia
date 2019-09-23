8a334c3ea7189d893ebc4a04af8de433160146b9
atagadtad <aarontagadtad@gmail.com>
2019-09-23 15:19:17 +0000

    made homePagePins.js within ./routes to hopefully retrieve all pins and return them into an array to use in the front page

---
+++ routes/homePagePins.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const app = express();
+
+
+module.exports = db => {
+  router.post("/", (req, res) => {
+    userEmail = req.body.email;
+    userPassword = req.body.password;
+    const values = [userEmail, userPassword]
+    db.query(`
+    SELECT *
+    FROM users
+    WHERE users.email = $1 AND users.password = $2;
+    `, values)
+      .then(data => {
+        const users = data.rows;
+        if (users[0].email === userEmail && users[0].password === userPassword) {
+          res.render("user");
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  return router;
+};
