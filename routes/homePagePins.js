d6af78631f8cb6a9e05536e5d8199832f8e59347
atagadtad <aarontagadtad@gmail.com>
2019-09-23 18:27:42 +0000

    cleaned and refactored code

--- routes/homePagePins.js
+++ routes/homePagePins.js
@@ -5,6 +5,7 @@ const app = express();
 
 module.exports = db => {
   router.get("/", (req, res) => {
+
     res.render("index");
   });
   router.get("/pins", (req, res) => {
@@ -24,7 +25,7 @@ module.exports = db => {
   });
 
   router.get('/pins', (req, res) => {
-    res.send({markers:  [{lat: 21.213213, ling: 43.4345}, {lat: 42.2323, ling: 12.344}]})
+    res.send({ markers: [{ lat: 21.213213, ling: 43.4345 }, { lat: 42.2323, ling: 12.344 }] })
   })
   return router;
 };
