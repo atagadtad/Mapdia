0f2b306df686abe890b28d45d060757aee5d87ba
atagadtad <aarontagadtad@gmail.com>
2019-09-24 15:19:31 +0000

    created new ./routes/logout.js for logout implementation.

---
+++ routes/logouts.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const cookieSession = require('cookie-session');
+
+router.use(cookieSession({
+  name: 'user_id',
+  keys: ['id']
+}));
+
+
+module.exports = db => {
+  router.post("/", (req, res) => {
+    res.clearCookie('user_id');
+    res.clearCookie('user_id.sig');
+    res.redirect("index");
+    db.query(`
+
+    `, values)
+      .then(data => {
+
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+  return router;
+};
