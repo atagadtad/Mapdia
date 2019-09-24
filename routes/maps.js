ec9cf91064ca07acae06f164627d4ab1a156dfc5
atagadtad <aarontagadtad@gmail.com>
2019-09-24 23:20:37 +0000

    debugged with console.log and will impelemnt query after push

--- routes/maps.js
+++ routes/maps.js
@@ -11,15 +11,26 @@ module.exports = db => {
       .then(data => {
         // console.log(data)
         const maps = data.rows;
-        console.log(maps);
+        // console.log(maps);
         res.json({ maps });
       })
       .catch(err => {
         res.status(500).json({ error: err.message });
       });
   });
-
-
+  router.post("/mapsubmission", (req, res) => {
+    console.log('hai')
+    db.query(`
+    SELECT *
+    FROM maps;
+    `)
+      .then(data => {
+        res.sendStatus(200)
+      })
+      .catch(err => {
+        res.status(500).json({ error: err.message });
+      });
+  })
 
 
   return router;
