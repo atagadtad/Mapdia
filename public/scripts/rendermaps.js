595daef2f8fca51990894491d0b221c018cefcd3
allisonnnnnn <allisonchen1130@gmail.com>
2019-09-25 16:58:51 -0400

    changed

--- public/scripts/rendermaps.js
+++ public/scripts/rendermaps.js
@@ -5,14 +5,9 @@ function insertAnchor(mapId, src) {
   let imgtag = insertImage(src);
   return `<a href="/showmap/${mapId}"><img src="${src}"></a>`;
 }
-<<<<<<< HEAD
-=======
-$(function ($) {
->>>>>>> 6915723d5a354658da6a375f4b5c39fc169995f2
 
 $(function($) {
   $.ajax({
-<<<<<<< HEAD
     url: "/maps",
     success: data => {
       for (let map of data.maps) {
@@ -21,39 +16,26 @@ $(function($) {
       }
     }
   });
-});
-=======
-    url: '/maps',
-    success: (data) => {
-      for (let map of data.maps) {
-        console.log(data.maps[0].url);
-        $('.mapsContainer').append(insertAnchor(map.id, map.url));
-      }
-    }
-  });
 
-  $("#searchbutton").click(function (event) {
+  $("#searchbutton").click(function(event) {
     event.preventDefault();
-    $('.mapsContainer').empty();
-    let text = $("#searchtext").val()
-    console.log('text: ', text)
+    $(".mapsContainer").empty();
+    let text = $("#searchtext").val();
+    console.log("text: ", text);
     $.ajax({
       type: "GET",
-      url: '/search',
+      url: "/search",
       data: { search: text },
-      success: (data) => {
-        console.log(data)
+      success: data => {
+        console.log(data);
         // console.log(data.maps[0].maps)
         for (let map of data.maps) {
           // console.log(data.maps[0].url);
-          $('.mapsContainer').append(insertAnchor(map.id, map.url));
+          $(".mapsContainer").append(insertAnchor(map.id, map.url));
         }
       }
-    })
+    });
 
     // $('.mapsContainer').append(insertAnchor(map.id, map.url));
-
-
-  })
-})
->>>>>>> 6915723d5a354658da6a375f4b5c39fc169995f2
+  });
+});
