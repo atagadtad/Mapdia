83bdae63ca216a164fb80dc05614b6c578c9db4a
atagadtad <aarontagadtad@gmail.com>
2019-09-25 15:14:12 +0000

    deleted commented out code

--- routes/logins.js
+++ routes/logins.js
@@ -10,47 +10,7 @@ router.use(
   })
 );
 
-// module.exports = db => {
-//   router.post("/", (req, res) => {
-//     let userEmail = req.body.email;
-//     let userPassword = req.body.password;
-//     console.log(userEmail);
-//     const values = [userEmail, userPassword];
-//     db.query(
-//       `
-//     SELECT *
-//     FROM users
-//     WHERE users.email = $1 AND users.password = $2;
-//     `,
-//       values
-//     )
-//       .then(data => {
-//         const users = data.rows;
-//         if (
-//           (
-//             users[0].email === userEmail &&
-//             users[0].password === userPassword
-//           )
-//           ||
-//           (
-//             users[0].email === userEmail &&
-//             bcrypt.compareSync(users[0].password, userPassword)
-//           )
-//         ) {
-//           req.session.user_id = users[0].id;
-//           let templateVars = {
-//             user: req.session.user_id
-//           };
-//           console.log(req.session.user_id);
-//           res.render("homepage", { name: "Bob", user: req.session.user_id });
-//         }
-//       })
-//       .catch(err => {
-//         res.status(500).json({ error: err.message });
-//       });
-//   });
-//   return router;
-// };
+
 module.exports = db => {
   router.post("/", (req, res) => {
     let userEmail = req.body.email;
@@ -73,10 +33,10 @@ module.exports = db => {
           };
         }
         if (req.session.user_id) {
-          res.render("homepage", { name: userEmail, user: req.session.user_id, error:'' });
+          res.render("homepage", { name: userEmail, user: req.session.user_id, error: '' });
           return;
         } else {
-          res.render("homepage", { name: '',user: '', error: "Please check email or password" });
+          res.render("homepage", { name: '', user: '', error: "Please check email or password" });
         }
       })
       .catch(err => {
