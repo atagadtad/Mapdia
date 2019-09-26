acc18d6b6646bdea33572efbca53aa6b9313b3ec
atagadtad <aarontagadtad@gmail.com>
2019-09-26 15:23:33 +0000

    deleted unused block of code

--- server/database.js
+++ server/database.js
@@ -1,24 +1,5 @@
 const db = require('../lib/db')
 
-
-/// Users
-
-/**
- * Get a single user from the database given their email.
- * @param {String} email The email of the user.
- * @return {Promise<{}>} A promise to the user.
- */
-
-const getUserWithEmail = function (email) {
-  const values = [`${email}`]
-  return db.query(`
-  SELECT *
-  FROM users
-  WHERE users.email = $1;
-  `, values)
-    .then(res => res.rows[0]);
-}
-
 const getPin = function (id) {
   const values = [`${id}`]
   return db.query(`
@@ -26,6 +7,6 @@ const getPin = function (id) {
   FROM pins
   WHERE pins.id = $1;
   `, values)
-  .then(res => res.rows[0]);
+    .then(res => res.rows[0]);
 }
 exports.getPin = getPin;
